4dbd496cbe7ffb6179ee09b1f8ce9490e712ff83
Vitaly Osipov <vital-nvl@mail.ru>
2022-12-05 18:21:57 +0300

    Work1

---
+++ Work1.py
@@ -0,0 +1,16 @@
+# Вычислить число Пи c заданной точностью d
+
+import math
+
+lis = [(math.pi)]
+
+#d = int(input'Введите точность для числа Pi: ')
+
+print(lis)
+
+i = int(input('input: '))
+
+l = lis[4:i]
+
+
+print(l)
